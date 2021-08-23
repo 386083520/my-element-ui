4dc101b1857b79f65430f8ddae95a7a731a46da8
386083520 <386083520@qq.com>
2021-08-23 19:38:21 +0800

    element-ui入口

---
+++ packages/button/src/button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button>
+        abc{{size}}
+    </button>
+</template>
+<script>
+    export default {
+        name: 'ElButton',
+        props: {
+            size: String
+        }
+    };
+</script>
