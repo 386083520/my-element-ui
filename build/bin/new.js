de97c8878da8701da0deac782baf8471ffe91f0e
386083520 <386083520@qq.com>
2021-08-25 08:28:11 +0800

    new.js自动生成包

--- build/bin/new.js
+++ build/bin/new.js
@@ -3,16 +3,67 @@ const componentname = process.argv[2];
 const ComponentName = uppercamelcase(componentname);
 console.log('gsd', componentname, ComponentName)
 
+const fs = require('fs');
 const path = require('path');
 const fileSave = require('file-save');
+const PackagePath = path.resolve(__dirname, '../../packages', componentname);
 
+
+const Files = [
+  {
+    filename: 'index.js',
+    content: `import ${ComponentName} from './src/main';
+
+/* istanbul ignore next */
+${ComponentName}.install = function(Vue) {
+  Vue.component(${ComponentName}.name, ${ComponentName});
+};
+
+export default ${ComponentName};`
+  },
+  {
+    filename: 'src/main.vue',
+    content: `<template>
+  <div class="el-${componentname}"></div>
+</template>
+
+<script>
+export default {
+  name: 'El${ComponentName}'
+};
+</script>`
+  },
+  {
+    filename: path.join('../../packages/theme-chalk/src', `${componentname}.scss`),
+    content: `@import "mixins/mixins";
+@import "common/var";
+
+@include b(${componentname}) {
+}`
+  }
+]
 const componentsFile = require('../../components.json');
 if (componentsFile[componentname]) {
     console.error(`${componentname} 已存在.`);
     process.exit(1);
 }
+
 componentsFile[componentname] = `./packages/${componentname}/index.js`;
+console.log(JSON.stringify(componentsFile, null, '  '))
 fileSave(path.join(__dirname, '../../components.json'))
     .write(JSON.stringify(componentsFile, null, '  '), 'utf8')
     .end('\n');
+
+// 添加到 index.scss
+const sassPath = path.join(__dirname, '../../packages/theme-chalk/src/index.scss');
+const sassImportText = `${fs.readFileSync(sassPath)}@import "./${componentname}.scss";`;
+fileSave(sassPath)
+    .write(sassImportText, 'utf8')
+    .end('\n');
+
+Files.forEach(file => {
+    fileSave(path.join(PackagePath, file.filename))
+        .write(file.content, 'utf8')
+        .end('\n');
+})
 
