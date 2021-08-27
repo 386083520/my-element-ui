da22b5913278761cd3acc57060d299d0eb7a9996
386083520 <386083520@qq.com>
2021-08-27 23:51:09 +0800

    alert组件实现

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -1,21 +1,42 @@
 <template>
-  <div class="el-alert"
-       :class="[typeClass, 'is-' + effect]"
-       v-show="visible">
-    <div class="el-alert__content">
-      <span>
-        <slot name="title">{{ title }}</slot>
-      </span>
-      <i class="el-alert__closebtn" :class="{ 'is-customed': closeText !== '', 'el-icon-close': closeText === '' }" @click="close()">{{closeText}}</i>
+  <transition name="el-alert-fade">
+    <div class="el-alert"
+         :class="[typeClass, center ? 'is-center' : '', 'is-' + effect]"
+         v-show="visible"
+         role="alert">
+      <i class="el-alert__icon" :class="[ iconClass, isBigIcon ]" v-if="showIcon"></i>
+      <div class="el-alert__content">
+        <span class="el-alert__title" :class="[ isBoldTitle ]" v-if="title || $slots.title">
+          <slot name="title">{{ title }}</slot>
+        </span>
+        <p class="el-alert__description" v-show="$slots.default && !description"><slot></slot></p>
+        <p class="el-alert__description" v-show="description && !$slots.default">{{ description }}</p>
+        <i class="el-alert__closebtn" :class="{ 'is-customed': closeText !== '', 'el-icon-close': closeText === '' }"  v-show="closable" @click="close()">{{closeText}}</i>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
-export default {
+const TYPE_CLASSES_MAP = {
+  'success': 'el-icon-success',
+  'warning': 'el-icon-warning',
+  'error': 'el-icon-error'
+};
+module.exports = {
   name: 'ElAlert',
   props: {
     title: {
+      type: String,
+      default: ''
+    },
+    closable: {
+      type: Boolean,
+      default: true
+    },
+    showIcon: Boolean,
+    center: Boolean,
+    description: {
       type: String,
       default: ''
     },
@@ -25,7 +46,10 @@ export default {
     },
     effect: {
       type: String,
-      default: 'light'
+      default: 'light',
+      validator: function(value) {
+        return ['light', 'dark'].indexOf(value) !== -1;
+      }
     },
     closeText: {
       type: String,
@@ -35,6 +59,15 @@ export default {
   computed: {
     typeClass() {
       return `el-alert--${ this.type }`;
+    },
+    iconClass() {
+      return TYPE_CLASSES_MAP[this.type] || 'el-icon-info';
+    },
+    isBigIcon() {
+      return this.description || this.$slots.default ? 'is-big' : '';
+    },
+    isBoldTitle() {
+      return this.description || this.$slots.default ? 'is-bold' : '';
     }
   },
   data() {
