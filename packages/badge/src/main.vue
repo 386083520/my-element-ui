2dfbe7a65fde5f6862aa4bea82dcbf63cfae42ca
386083520 <386083520@qq.com>
2021-09-03 22:20:39 +0800

    badge组件实现

--- packages/badge/src/main.vue
+++ packages/badge/src/main.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="el-badge">
-    <slot></slot>
-    <sup
-        v-show="!hidden && (content || content === 0 || isDot)"
-        v-text="content">
-    </sup>
-  </div>
+    <div class="el-badge">
+      <slot></slot>
+      <transition name="el-zoom-in-center">
+        <sup
+                v-show="!hidden && (content || content === 0 || isDot)"
+                v-text="content"
+                class="el-badge__content"
+                :class="['el-badge__content--' + type, {
+              'is-fixed': $slots.default,
+              'is-dot': isDot
+            }]">
+        </sup>
+      </transition>
+    </div>
 </template>
 
 <script>
@@ -15,7 +22,13 @@ module.exports = {
     value: [String, Number],
     max: Number,
     hidden: Boolean,
-    isDot: Boolean
+    isDot: Boolean,
+    type: {
+      type: String,
+      validator(val) {
+        return ['primary', 'success', 'warning', 'info', 'danger'].indexOf(val) > -1;
+      }
+    }
   },
   computed: {
     content() {
