93d91f275bce48227bb79fc22cdd1f3080bdad98
386083520 <386083520@qq.com>
2021-09-07 07:57:52 +0800

    demo-block引入

---
+++ examples/components/demo-block.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="source">
+            <slot name="source"></slot>
+        </div>
+        <div class="meta" ref="meta">
+            <div class="description" v-if="$slots.default">
+                <slot></slot>
+            </div>
+            <div class="highlight">
+                <slot name="highlight"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: ""
+    }
+</script>
+
+<style scoped>
+
+</style>
--- examples/entry.js
+++ examples/entry.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import entry from './app';
+import Element from '../src/index.js';
 import VueRouter from 'vue-router';
 import MainFooter from './components/footer';
 import MainHeader from './components/header';
+import demoBlock from './components/demo-block';
 import routes from './route.config'
+
+import '../packages/theme-chalk/src/index.scss';
 import './assets/styles/common.css';
 
+Vue.use(Element);
 Vue.use(VueRouter);
+Vue.component('demo-block', demoBlock);
 Vue.component('main-footer', MainFooter);
 Vue.component('main-header', MainHeader);
 
